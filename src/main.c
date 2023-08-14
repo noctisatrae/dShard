714c1e64fe264bf1b66d8b7509bfac989f0295f5
noctisatrae <pro.hector.kub@gmail.com>
2023-08-15 00:57:18 +0200

    change lil detail

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ int main() {
   int number1 = 3;
   int number2 = 6;
 
-  dObject* data = create_data(1, 0, 0, &number1, sizeof(number1));
+  dObject* data = create_data(0, 0, 0, &number1, sizeof(number1));
   dObject* data2 = create_data(0, 0, 0,&number2, sizeof(number1));
 
   Node* node = create_node("0x0", data);
