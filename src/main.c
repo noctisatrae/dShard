e099e8c7dc276badf2f71d4c7b76518a87352091
noctisatrae <pro.hector.kub@gmail.com>
2023-08-14 23:20:00 +0200

    better than nothing

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@ int main() {
   int number2 = 6;
 
   dObject* data = create_data(0, 0, 0, (void*)(intptr_t)number1);
-  dObject* data2 = create_data(333, 0, 0, (void*)(intptr_t)number2);
+  dObject* data2 = create_data(0, 0, 0, (void*)(intptr_t)number2);
 
   Node* node = create_node("0x0", data);
   add_data(node, data2);
