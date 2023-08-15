13e401f6a556f0ed9e48fb7c80d3889aaa4188df
noctisatrae <pro.hector.kub@gmail.com>
2023-08-15 18:52:32 +0200

    fix dumb error

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@ int main() {
   int number2 = 6;
 
   dObject* data = create_data(1, 1, 0, &number1, sizeof(number1));
-  dObject* data2 = create_data(0, 2, 0,&number2, sizeof(number1));
+  dObject* data2 = create_data(0, 2, 0,&number2, sizeof(number2));
 
   Node* node = create_node("0x0", data);
   add_data(node, *data2);
